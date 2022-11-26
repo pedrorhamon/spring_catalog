7dba745ff3068e882c282922a83ce17a3eb1c6f2
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-26 07:39:26 -0300

    H2 camadas de dados e bd

--- src/main/java/com/starking/dscatalog/controller/CategoryController.java
+++ src/main/java/com/starking/dscatalog/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.starking.dscatalog.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.dscatalog.domain.Category;
+import com.starking.dscatalog.services.CategoryService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,14 +17,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CategoryController {
 	
+	private final CategoryService categoryService;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electonics"));
-		list.add(new Category(3L, "Manual"));
-		
+		List<Category> list = this.categoryService.findAll();
 		return ResponseEntity.ok(list);
 	}
-
 }
--- src/main/java/com/starking/dscatalog/services/CategoryService.java
+++ src/main/java/com/starking/dscatalog/services/CategoryService.java
@@ -1,11 +1,22 @@
 package com.starking.dscatalog.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.starking.dscatalog.domain.Category;
+import com.starking.dscatalog.repository.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
+	
+	private final CategoryRepository categoryRepository;
+	
+	public List<Category> findAll() {
+		return this.categoryRepository.findAll();
+	}
 
 }

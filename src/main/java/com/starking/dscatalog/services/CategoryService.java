d4c1999fb3ac70fd15f8fd8e11562688a7a6b6ae
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-26 07:23:57 -0300

    Criando padroes MVC

---
+++ src/main/java/com/starking/dscatalog/services/CategoryService.java
@@ -0,0 +1,11 @@
+package com.starking.dscatalog.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+}

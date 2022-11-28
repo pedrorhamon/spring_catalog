8781cc77176c065708a2a38e3430fc44089b75b2
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-28 18:44:57 -0300

    Criando DTOs para Category

---
+++ src/main/java/com/starking/dscatalog/domain/dtos/CategoryDTO.java
@@ -0,0 +1,16 @@
+package com.starking.dscatalog.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+	
+	private Long id;
+	private String nome;
+}

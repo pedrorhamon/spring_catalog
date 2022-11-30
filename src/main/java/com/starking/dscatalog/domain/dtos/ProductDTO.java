3dc95c4d866c03b5c17d673f51a9634ee7e46222
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-30 17:00:43 -0300

    dto criando de Product

---
+++ src/main/java/com/starking/dscatalog/domain/dtos/ProductDTO.java
@@ -0,0 +1,9 @@
+package com.starking.dscatalog.domain.dtos;
+
+import java.io.Serializable;
+
+public class ProductDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}

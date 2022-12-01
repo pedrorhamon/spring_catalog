f69e88d4dc03b87da0192d4c87f97af37c06f0c0
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-12-01 18:47:07 -0300

    Criando DTO de Product e implementando

--- src/main/java/com/starking/dscatalog/domain/dtos/ProductDTO.java
+++ src/main/java/com/starking/dscatalog/domain/dtos/ProductDTO.java
@@ -1,9 +1,48 @@
 package com.starking.dscatalog.domain.dtos;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import com.starking.dscatalog.domain.Category;
+import com.starking.dscatalog.domain.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	private Long id;
+	private String name;
+	private BigDecimal price;
+	private String description;
+	private Instant date;
+	private String imgUrl;
+	
+	private List<CategoryDTO> categories = new ArrayList<>();
+ 	
+	public ProductDTO(Product entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price = entity.getPrice();
+		this.imgUrl = entity.getImgUrl();
+		this.date = entity.getDate();
+	}
+	
+	public ProductDTO(Product entity, Set<Category> categories) {
+		this(entity);
+		categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
+	}
 }

de36442b34e097e986dd05c5a21df83fa9ce3663
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-12-02 21:21:55 -0300

    dois cenario de deleteById

--- src/test/java/com/starking/dscatalog/repository/ProductRepositoryTest.java
+++ src/test/java/com/starking/dscatalog/repository/ProductRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import com.starking.dscatalog.domain.Product;
 
@@ -23,5 +24,13 @@ public class ProductRepositoryTest {
 		
 		Optional<Product> product = this.productRepository.findById(exintigId);
 		Assertions.assertFalse(product.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldDeleteThrowDataIntegrityViolationException() {
+		long nonExintigId = 1000L;
+		Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+			this.productRepository.deleteById(nonExintigId);
+		});
 	}
 }

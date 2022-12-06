a83032c6330920d7f629c0d8af3d99092881cdca
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-12-06 18:55:39 -0300

    Inicial de UserDTO criando a class

---
+++ src/main/java/com/starking/dscatalog/domain/dtos/UserDTO.java
@@ -0,0 +1,16 @@
+package com.starking.dscatalog.domain.dtos;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}

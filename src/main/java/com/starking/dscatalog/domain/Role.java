3ae486737196855f7c686edaa5e703ea199ff9bd
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-12-06 19:15:22 -0300

    Criando a entity Role

---
+++ src/main/java/com/starking/dscatalog/domain/Role.java
@@ -0,0 +1,27 @@
+package com.starking.dscatalog.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "role")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String authority;
+}

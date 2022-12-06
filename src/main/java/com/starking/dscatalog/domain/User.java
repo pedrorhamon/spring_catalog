b0a751b611b54cdf90ba3724ca1196169de68ea0
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-12-06 18:54:03 -0300

    implementando a entidade User

--- src/main/java/com/starking/dscatalog/domain/User.java
+++ src/main/java/com/starking/dscatalog/domain/User.java
@@ -2,8 +2,27 @@ package com.starking.dscatalog.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
 
 }

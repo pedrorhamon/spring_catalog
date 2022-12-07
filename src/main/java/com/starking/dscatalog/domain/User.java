7e7ec8e7c9b4fc9e926941e2b673d2a0a3b0afc2
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-12-07 17:36:28 -0300

    anotaçoes para User a entidade

--- src/main/java/com/starking/dscatalog/domain/User.java
+++ src/main/java/com/starking/dscatalog/domain/User.java
@@ -14,13 +14,17 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tb_user")
 public class User implements Serializable {
 
